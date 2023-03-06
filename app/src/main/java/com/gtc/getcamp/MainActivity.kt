b20a87ffb96c8a15c0142d735d2cfde8b5f712d8
OmerAKSU <omeraksu@teknasyon.com>
2023-03-06 23:00:10 +0300

    - MainActivity was refactored for theming - MainViewModel was created to collect user pref.

--- app/src/main/java/com/gtc/getcamp/MainActivity.kt
+++ app/src/main/java/com/gtc/getcamp/MainActivity.kt
@@ -3,33 +3,67 @@ package com.gtc.getcamp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.gtc.getcamp.navigator.Navigator
 import com.gtc.getcamp.navigator.NavigatorGraphApi
 import com.gtc.getcamp.navigator.registerGraph
 import com.gtc.getcamp.ui.theme.GetcampTheme
+import com.gtc.samples.getcamp.feature.settings.domain.model.ThemeConfig
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var navigator: Navigator
 
+    val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var uiState: MainActivityUiState by mutableStateOf(MainActivityUiState.Loading)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }.collect()
+            }
+        }
+
         setContent {
+            val darkTheme = shouldShowInDarkTheme(uiState)
+
             AppGraph(
                 navigator = navigator,
                 childGraphs = emptyArray(),
                 startDestination = "TODO please provide start destinationÏ"
             )
-            GetcampTheme {
+            GetcampTheme(
+                darkTheme = darkTheme
+            ) {
 
             }
         }
     }
+
+
 }
 
 @Composable
@@ -43,5 +77,19 @@ fun AppGraph(
 
     NavHost(navController = navController, startDestination = startDestination) {
         registerGraph(*childGraphs)
+    }
+}
+
+@Composable
+private fun shouldShowInDarkTheme(
+    uiState: MainActivityUiState,
+): Boolean {
+    return when (uiState) {
+        is MainActivityUiState.Loading -> isSystemInDarkTheme()
+        is MainActivityUiState.Success -> when (uiState.themeConfig) {
+            ThemeConfig.FOLLOW_SYSTEM -> isSystemInDarkTheme()
+            ThemeConfig.DARK -> false
+            ThemeConfig.LIGHT -> true
+        }
     }
 }
---
+++ app/src/main/java/com/gtc/getcamp/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package com.gtc.getcamp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gtc.samples.getcamp.feature.settings.domain.model.ThemeConfig
+import com.gtc.samples.getcamp.feature.settings.domain.usecase.UserPrefUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    userPrefUseCase: UserPrefUseCase,
+) : ViewModel() {
+
+    val uiState: StateFlow<MainActivityUiState> = userPrefUseCase.getThemeConfig().map {
+        MainActivityUiState.Success(it)
+    }.stateIn(
+        scope = viewModelScope,
+        initialValue = MainActivityUiState.Loading,
+        started = SharingStarted.WhileSubscribed(5_000),
+    )
+}
+
+sealed interface MainActivityUiState {
+    object Loading : MainActivityUiState
+    data class Success(val themeConfig: ThemeConfig) : MainActivityUiState
+}
